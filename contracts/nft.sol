9caead060ce93425667f8230b165eb6633a2a2f9
Gopal Ramesh Dahale <49199003+Gopal-Dahale@users.noreply.github.com>
2021-12-06 23:48:25 +0530

    update nft.sol

--- contracts/nft.sol
+++ contracts/nft.sol
@@ -4,13 +4,16 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 
+// Inherit ERC721URIStorage which also extends ERC721
 contract SimpleNFT is ERC721URIStorage {
-    uint256 public counter_tok;
-
+    uint256 public counter_tok; // Counter for tokens
+    
+    // Name and symbol
     constructor() ERC721("SimpleNFT", "NFT") {
-        counter_tok = 0;
+        counter_tok = 0; // Initialize to zero
     }
-
+    
+    // Function to mint NFT
     function mint(string memory tokenURI) public returns (uint256)
     {
         uint256 new_item_id = counter_tok;
